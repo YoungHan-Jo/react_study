8239882729d5837e9ddf374d8cea7075e7537520
YoungHan Jo <verin4494@gmail.com>
2023-06-18 18:31:14 +0900

    fetch api

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import NewPost from './NewPost';
 import Post from './Post';
@@ -7,8 +7,28 @@ import Modal from './Modal';
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false); // loading spinnerを表示するためのstate
+
+  // fetchをそのまま使うと中にあるsetPostsで無限ループになるので、useEffectを使う
+  useEffect(() => {
+    //useEffect hookは async/awaitを使えない -> 中にasync functionを定義する
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch('http://localhost:8080/posts');
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+  }, []); //　２nd argにいつ実行するかを指定する。空の配列を指定すると、最初の１回だけ実行される
 
   function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: { 'Content-Type': 'application/json' },
+    });
     setPosts(existingPosts =>
       // 最新のstateのsnapshotをexistingPostsでもらう
       [postData, ...existingPosts],
@@ -22,7 +42,7 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map(post => (
             <Post key={post.body} author={post.author} body={post.body} />
@@ -31,10 +51,15 @@ function PostsList({ isPosting, onStopPosting }) {
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <p>Loading posts ...</p>
         </div>
       )}
     </>

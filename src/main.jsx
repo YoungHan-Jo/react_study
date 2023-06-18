6325f1993247159288f51982f835fcf18f2eb66b
YoungHan Jo <verin4494@gmail.com>
2023-06-18 18:50:24 +0900

    route layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import App from './App';
 import './index.css';
+import NewPost from './components/NewPost';
+import RootLayout from './routes/RootLayout';
+
+const router = createBrowserRouter([
+  {
+    paht: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <App /> },
+      { path: '/create-post', element: <NewPost /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */} {/* App componentの代わりに　RouterProviderを使う */}
   </React.StrictMode>,
 );
